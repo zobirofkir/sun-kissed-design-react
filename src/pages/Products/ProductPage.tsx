9d1679ccf97325f665076641da88c489227d1e50
csw <zobirofkir19@gmail.com>
2025-05-13 15:47:29 +0100

    create products screen

--- src/pages/Products/ProductPage.tsx
+++ src/pages/Products/ProductPage.tsx
@@ -1,8 +1,23 @@
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
+import { Products } from '@/components/Products'
 import React from 'react'
 
 const ProductPage = () => {
   return (
-    <div>ProductPage</div>
+    <section>
+        <div>
+            <Navbar />
+        </div>
+
+        <div>
+            <Products />
+        </div>
+
+        <div>
+            <Footer />
+        </div>
+    </section>
   )
 }
 
